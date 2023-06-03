a31c8c2d14d9a1ee8309d9f1259340757fbb8a31
JayavelAnandh <jvanandh984@gmail.com>
2023-06-03 10:28:41 +0530

    edited response to provide username

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -1,32 +1,32 @@
-import express from 'express';
-import { User } from '../models/users.js';
-import bcrypt from 'bcrypt'
-import { generateAuthToken } from '../models/repoCreate.js';
+import express from "express";
+import { User } from "../models/users.js";
+import bcrypt from "bcrypt";
+import { generateAuthToken } from "../models/repoCreate.js";
 const router = express.Router();
 
-router.post("/",async(req,res)=>{
-    try {
-        let newUser = await User.findOne({email:req.body.email});
+router.post("/", async (req, res) => {
+  try {
+    let newUser = await User.findOne({ email: req.body.email });
 
-        if(newUser){
-            return res.status(400).send({message:"Email-id already exists"})
-        }
-
-        const salt =await bcrypt.genSalt(10);
-        const plainPasword = req.body.password.toString();
-        const hashedPassword = await bcrypt.hash(plainPasword,salt);
-         newUser = await new User({
-            userName:req.body.userName,
-            email:req.body.email,
-            password:hashedPassword
-        }).save()
-
-        const authToken = await generateAuthToken(newUser._id);
-        res.status(200).send({AuthToken:authToken})
-    } catch (error) {
-        console.log(error)
-        res.status(500).send();
+    if (newUser) {
+      return res.status(400).send({ message: "Email-id already exists" });
     }
-})
+
+    const salt = await bcrypt.genSalt(10);
+    const plainPasword = req.body.password.toString();
+    const hashedPassword = await bcrypt.hash(plainPasword, salt);
+    newUser = await new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPassword,
+    }).save();
+
+    const authToken = await generateAuthToken(newUser._id);
+    res.status(200).send({ AuthToken: authToken, userName: newUser.userName });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
 
 export const signUpRoute = router;
